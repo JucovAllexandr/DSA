9faa873724b63a6cd61c83c5395cb052a93d48f1
JucovAllexandr <sania337@gamil.com>
2019-11-05 19:06:24 +0200

    .

---
+++ DSA/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Org.BouncyCastle.Math;
+
+namespace DSA
+{
+    public class Helper
+    {
+        static public Org.BouncyCastle.Math.BigInteger GenerateBigIntegerPrimes(int bits)
+        {
+            Org.BouncyCastle.Security.SecureRandom ran = new Org.BouncyCastle.Security.SecureRandom();
+            Org.BouncyCastle.Math.BigInteger c = new Org.BouncyCastle.Math.BigInteger(bits, ran);
+            
+            for (; ; )
+            {
+                if (c.IsProbablePrime(100) == true) break;
+                c = c.Subtract(new Org.BouncyCastle.Math.BigInteger("1"));
+            }
+            return (c);
+        }
+
+        static public Org.BouncyCastle.Math.BigInteger GenerateBigIntegerP(int bits, BigInteger q)
+        {
+            Org.BouncyCastle.Security.SecureRandom ran = new Org.BouncyCastle.Security.SecureRandom();
+            BigInteger c = new BigInteger(bits, ran);
+
+            while (true)
+            {
+               // Console.WriteLine("c"+c);
+               // Console.WriteLine("q"+q);
+               // Console.WriteLine("mod"+c.Mod(q));
+                if (c.Mod(q).Equals(BigInteger.Zero))
+                {
+                    break;
+                }
+                //c = c.Subtract(new Org.BouncyCastle.Math.BigInteger("1"));
+                c = c.Subtract(q);
+            }
+            Console.WriteLine("c"+c);
+            Console.WriteLine("q"+q);
+            return c.Add(BigInteger.One);
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Math;
 
 namespace DSA
 {
@@ -6,7 +11,16 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte[] bytes = Encoding.UTF8.GetBytes("My Content Thanks");
+            SHA1 sha1 = new SHA1Managed();
+            Console.Out.WriteLine(BitConverter.ToString(sha1.ComputeHash(bytes)).Replace("-",""));
+
+            BigInteger q = Helper.GenerateBigIntegerPrimes(160);
+            Console.WriteLine("q = "+q);
+            BigInteger p = Helper.GenerateBigIntegerP(1024, q);
+            
+            Console.WriteLine("p = "+p);
         }
     }
 }
+
